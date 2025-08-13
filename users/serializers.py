ddac12cf2b76b3ef66f084928124ad432ed5c135
brahimNessal <nessaldjelloud047@gmail.com>
2025-08-13 11:25:16 +0100

    Fix Cloudinary config and return full image URL

--- users/serializers.py
+++ users/serializers.py
@@ -3,11 +3,17 @@ from .models import Dish
 
 class DishSerializer(serializers.ModelSerializer):
     chef_username = serializers.SerializerMethodField()
+    image_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Dish
-        fields = ('id', 'name', 'image', 'quantity', 'chef', 'chef_username', 'created_at')
+        fields = ('id', 'name', 'image', 'image_url', 'quantity', 'chef', 'chef_username', 'created_at')
         read_only_fields = ['id','chef', 'chef_username', 'created_at']
 
     def get_chef_username(self, obj):
         return obj.chef.username
+
+    def get_image_url(self, obj):
+        if obj.image:
+            return obj.image.url
+        return None
