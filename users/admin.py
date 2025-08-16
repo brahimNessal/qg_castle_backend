627f80ed03180f327c0b116c9f97235599b4518e
brahimNessal <nessaldjelloud047@gmail.com>
2025-08-16 14:46:23 +0100

    Fix Cloudinary config and return full image URL

--- users/admin.py
+++ users/admin.py
@@ -1,34 +1,40 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import Dish
-from .models import User
+from .models import Dish, User
 
 User = get_user_model()
 
-# جرب إلغاء تسجيله فقط إذا كان مسجلاً بالفعل (بشكل آمن)
+# إلغاء التسجيل القديم إذا كان موجود
 try:
     admin.site.unregister(User)
 except admin.sites.NotRegistered:
-    pass  # تجاهل إذا لم يكن مسجلاً أصلاً
+    pass
 
-# الآن سجل موديل المستخدم من جديد
 @admin.register(User)
 class CustomUserAdmin(BaseUserAdmin):
-    list_display = ('id', 'username', 'is_chef', 'is_staff', 'is_superuser', 'is_ingredient_manager')
-    list_filter = ('is_chef', 'is_staff', 'is_superuser', 'is_ingredient_manager')
+    list_display = ('id', 'username', 'is_chef', 'is_ingredient_manager', 'is_staff', 'is_superuser')
+    list_filter = ('is_chef', 'is_ingredient_manager', 'is_staff', 'is_superuser')
     search_fields = ('username',)
     ordering = ('id',)
 
-    fieldsets = BaseUserAdmin.fieldsets + (
-        (None, {'fields': ('is_chef', 'is_ingredient_manager')}),
+    # fieldsets الأصلي فيه is_staff, is_superuser إلخ
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', 'is_chef', 'is_ingredient_manager')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
 
-    add_fieldsets = BaseUserAdmin.add_fieldsets + (
-        (None, {'fields': ('is_chef', 'is_ingredient_manager')}),
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'is_chef', 'is_ingredient_manager', 'is_staff', 'is_superuser'),
+        }),
     )
 
-# تسجيل موديل الأطباق
+
+# تسجيل الأطباق
 @admin.register(Dish)
 class DishAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'quantity', 'chef', 'created_at')
